f055086348506ca882ac461477670568e1f1777b
Afiyo Gounoubou <nadfrancine@yahoo.fr>
2023-06-28 12:11:29 -0500

    first commit

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "profile" {
+  default = "default"
+}
+variable "topic_name" {
+  default = "sns-dev"
+}
+variable "Team" {
+  default = "dev"
+}
+variable "protocol_type" {
+  default = "email"
+}
+variable "end_point" {
+  default = "ghfgfhgsf@gmail.com"
+
+}
+variable "my_region" {
+    default = "us-east-1"
+  
+}
